193e9c4abfa10a29aba846496e116a6cd0593575
Bryan Jericho <bryanpanggalo@gmail.com>
2023-10-08 15:52:41 +0800

    Portofolio

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+const typingEffect = document.querySelector(".typing-effect");
+
+// // Tambahkan event listener pada elemen yang mengatur munculnya teks
+// document.addEventListener("DOMContentLoaded", () => {
+//   setTimeout(() => {
+//     typingEffect.classList.add("show");
+//   }, 500); // Ubah nilai 1000 sesuai dengan waktu yang Anda inginkan sebelum teks muncul
+// });
